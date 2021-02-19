c545e922e29200a2ad30caff261410f60e0db5db
figo5530 <figooooo0221@gmail.com>
2021-02-18 19:10:57 -0500

    commit tree fa955f14e2fc44b0ca88e043bca1e1cbc4f9502c

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Yinghui"
+    end
+    
+    get '/hometown' do 
+        "My hometown is Huangshi China"
+    end
+    
+    get '/favorite-song' do 
+        "My favorite song is 'You're beautiful'"
+    end
 end
